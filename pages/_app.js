fb28bbed662b06011e97403e84e2f2aaa0e6b37f
FChoudat <frederic@choudat.fr>
2025-03-15 16:26:28 +0100

    Adding header and css

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '@styles/globals.css'
+import Header from '../components/Header'
 
 function Application({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default Application
