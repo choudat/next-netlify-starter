b7d75a998e131d15768698614ebbfbaa21f0b884
FChoudat <frederic@choudat.fr>
2025-03-21 23:24:23 +0100

    Add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { initializeFaroMonitoring } from "../utils/faro";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -12,6 +13,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
